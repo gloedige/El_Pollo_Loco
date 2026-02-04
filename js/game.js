a8ac8868428d4e31a65b2a4923a2c05b50e19bfb
gloedige <gerd.loedige@posteo.de>
2026-02-04 11:06:21 +0100

    create instance of world

--- js/game.js
+++ js/game.js
@@ -1,13 +1,19 @@
 let canvas;
-let ctx;
-let character = new Image();
+let world;
+
 
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
-    character.src = "../img/2_character_pepe/2_walk/W-21.png";
 
-    ctx.drawImage(character, 30, 400 - 120, 50, 120);
+    // ctx = canvas.getContext('2d');
+
+    console.log('My Character is: ', world.character);
+    
+
+    // character.src = "../img/2_character_pepe/2_walk/W-21.png";
+
+    // ctx.drawImage(character, 30, 400 - 120, 50, 120);
 }
