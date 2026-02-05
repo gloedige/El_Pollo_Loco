ceef3138c154fe3734a9ab92fbaa6af808327f48
gloedige <gerd.loedige@posteo.de>
2026-02-05 16:41:19 +0100

    create class Level

---
+++ models/levels.class.js
@@ -0,0 +1,11 @@
+class Level {
+    enemies;
+    backgroundObjects;
+    clouds;
+
+    constructor(enemies, backgroundObjects, clouds) {
+        this.enemies = enemies;
+        this.backgroundObjects = backgroundObjects;
+        this.clouds = clouds;
+    }
+}
