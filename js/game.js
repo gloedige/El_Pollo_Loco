870e01cc9e483878adfd7ea63ee16122d6ce199c
gloedige <gerd.loedige@posteo.de>
2026-02-05 09:46:06 +0100

    create eventlistener for keyboard input

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
 
     // ctx = canvas.getContext('2d');
@@ -17,3 +18,22 @@ function init() {
 
     // ctx.drawImage(character, 30, 400 - 120, 50, 120);
 }
+
+
+window.addEventListener("keydown", (e) => {
+    // console.log('Key down: ', e.key);
+    if (e.key === "ArrowLeft") keyboard.LEFT = true;
+    if (e.key === "ArrowRight") keyboard.RIGHT = true;
+    if (e.key === "ArrowUp") keyboard.UP = true;
+    if (e.key === "ArrowDown") keyboard.DOWN = true;
+    if (e.key === " ") keyboard.SPACE = true;
+});
+
+window.addEventListener("keyup", (e) => {
+    // console.log('Key up: ', e.key);
+    if (e.key === "ArrowLeft") keyboard.LEFT = false;
+    if (e.key === "ArrowRight") keyboard.RIGHT = false;
+    if (e.key === "ArrowUp") keyboard.UP = false;
+    if (e.key === "ArrowDown") keyboard.DOWN = false;
+    if (e.key === " ") keyboard.SPACE = false;
+});
