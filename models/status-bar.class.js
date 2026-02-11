eb1512d15c49fa8948393ae7916ba0bbefca3a70
gloedige <gerd.loedige@posteo.de>
2026-02-11 06:57:02 +0100

    update class statusbar

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,8 +1,4 @@
 class StatusBar extends DrawableObject {
-    x = 10;
-    y = 10;
-    width = 200;
-    height = 60;
     percentage = 100;
     STATUS_BAR_IMAGES = [
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
@@ -12,32 +8,41 @@ class StatusBar extends DrawableObject {
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
-
+    
     offset = {
         top: 0,
         left: 0,
         right: 0,
         bottom: 0
-     };
-
+    };
+    
     constructor() {
-        super().loadImage(this.STATUS_BAR_IMAGES[5]);
+        super();
         this.loadImages(this.STATUS_BAR_IMAGES);
-        let self = this;
+        this.setPercentage(100);
+        this.x = 10;
+        this.y = 0;
+        this.width = 200;
+        this.height = 60;
     }
 
     setPercentage(percentage) {
-        if (percentage <= 0) {
-            return 0;
-        } else if (percentage >= 100) {
-            return 5;
-        }
         this.percentage = percentage;
-        let index = Math.floor(percentage / 20);
-        if (index < 0) index = 0;
-        if (index >= this.STATUS_BAR_IMAGES.length) index = this.STATUS_BAR_IMAGES.length - 1;
-        this.img = this.imageCache[this.STATUS_BAR_IMAGES[index]];
+        let path = this.STATUS_BAR_IMAGES[this.getIndexOfImageForPercentage()];
+        this.img = this.imagesCache[path];
+
     }
 
+    getIndexOfImageForPercentage() {
+        if (this.percentage <= 0) {
+            return 0;
+        } else if (this.percentage >= 100) {
+            return 5;
+        }
+        let index = Math.floor(this.percentage / 20);
+        if (index < 0) index = 0;
+        if (index >= this.STATUS_BAR_IMAGES.length) index = this.STATUS_BAR_IMAGES.length - 1;
+        return index;
+    }
 
-}
+    }
