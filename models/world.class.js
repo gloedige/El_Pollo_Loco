30f7305b94c67b302290feefe2f6e397474ed9df
gloedige <gerd.loedige@posteo.de>
2026-02-27 09:50:54 +0100

    update world with clean code and js doc

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,9 @@
+/**
+ * Represents the game world, managing the main game loop, rendering, and interactions.
+ */
 class World {
     character = new Character();
-    statusBarHealth = new StatusBar(10, 0, 'health', 100, this.character.energy);
+    statusBarHealth = new StatusBar(10, 0, 'health', this.character.TOTAL_ENERGY, this.character.energy);
     statusBarCoins = new StatusBar(10, 40, 'coins', 0, numberOfCoins);
     statusBarBottles = new StatusBar(10, 80, 'bottles', 0, numberOfBottles);
     throwableObjects = [];
@@ -15,7 +18,14 @@ class World {
     camera_x = 0;
     positionCharacterInWorld_x = 100;
     widthOfSingleBackground = 719;
-    
+ 
+
+    /**
+     * Creates a new World instance and initializes the game.
+     * @param {HTMLCanvasElement} canvas - The canvas element.
+     * @param {Keyboard} keyboard - The keyboard input handler.
+     * @param {Object} sounds - The sounds manager.
+     */
     constructor(canvas, keyboard, sounds) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -28,10 +38,14 @@ class World {
         this.run();
     }
 
+
+    /**
+     * Starts the main game loop and collision checks.
+     */
     run(){
         let interval_checkCollisions = setInterval(() => {
             this.checkThrowObjects();
-            this.checkCollisionsOfCharacter();
+            this.checkCollisionsHandler();
         }, 1000/10);
         let interval_checkJumpingOnTop = setInterval(() => {
             this.checkJumpingOnTop();
@@ -40,38 +54,68 @@ class World {
         window.activeIntervals.push(interval_checkCollisions, interval_checkJumpingOnTop);
     }
 
-    
-    checkCollisionsOfCharacter() {
+
+    /**
+     * Handles all collision checks in the world.
+     */
+    checkCollisionsHandler() {
         if (this.level instanceof Level) {
-            this.level.enemies.forEach(enemy => {
-                if (this.character.colliding_detecting && this.character.isColliding(enemy) && !this.character.isJumpingOnTop(enemy) && !enemy.dead && !this.character.isHit()) {
-                    this.character.hit();
-                    this.statusBarHealth.setPercentage(this.character.energy);
-                    this.character.checkIsDead();
-                }
-            });
-            this.level.coins.forEach(coin => {
-                if (this.character.colliding_detecting && this.character.isColliding(coin)) {
-                    this.character.collectCoin();
-                    this.playCollectCoinSound();
-                    this.statusBarCoins.setPercentage(this.character.coinsCollected);
-                    coin.colliding_detecting = false;
-                    this.character.deleteElement(coin);
-                }
-            });
-            this.level.bottles.forEach(bottle => {
-                if (this.character.colliding_detecting && this.character.isColliding(bottle)) {
-                    this.character.collectBottles();
-                    this.playCollectBottleSound();
-                    this.updateStatusBarBottles();
+            this.handleCollisionsWithEnemies();
+            this.handleCollisionWithCoins();
+            this.handleCollisionWithBottles();
+        }
+    }
+
+
+    /**
+     * Handles collisions between the character and enemies.
+     */
+    handleCollisionsWithEnemies() {
+        this.level.enemies.forEach(enemy => {
+            if (this.character.colliding_detecting && this.character.isColliding(enemy) && !this.character.isJumpingOnTop(enemy) && !enemy.dead && !this.character.isHit()) {
+                this.character.hit();
+                this.statusBarHealth.setPercentage(this.character.energy);
+                this.character.checkIsDead();
+            };
+        });
+    }
+
+
+    /**
+     * Handles collisions between the character and coins.
+     */
+    handleCollisionWithCoins() {
+        this.level.coins.forEach(coin => {
+            if (this.character.colliding_detecting && this.character.isColliding(coin)) {
+                this.character.collectCoin();
+                this.playCollectCoinSound();
+                this.statusBarCoins.setPercentage(this.character.coinsCollected);
+                coin.colliding_detecting = false;
+                this.character.deleteElement(coin);
+            };
+        });
+    }
+
+
+    /**
+     * Handles collisions between the character and bottles.
+     */
+    handleCollisionWithBottles() {
+        this.level.bottles.forEach(bottle => {
+            if (this.character.colliding_detecting && this.character.isColliding(bottle)) {
+                this.character.collectBottles();
+                this.playCollectBottleSound();
+                this.updateStatusBarBottles();
                     bottle.colliding_detecting = false;
                     this.character.deleteElement(bottle);                    
                 }
             });
-        }
     }
-    
-    
+
+
+    /**
+     * Checks if the character is jumping on top of an enemy.
+     */
     checkJumpingOnTop() {
         if (this.level instanceof Level) {
             this.level.enemies.forEach(enemy => {
@@ -85,6 +129,10 @@ class World {
         }
     }
 
+
+    /**
+     * Checks if an enemy is hit by a thrown bottle.
+     */
     checkHitByBottle() {
          if (this.level instanceof Level) {
             this.throwableObjects.forEach(bottle => {
@@ -93,17 +141,29 @@ class World {
                         enemy.hit();
                         enemy.checkIsDead();
                         bottle.colliding_detecting = false;
-                        const index = this.throwableObjects.indexOf(bottle);
-                        if (index > -1) {
-                            this.throwableObjects.splice(index, 1);
-                        }
+                        this.handleDeleteThrowableObject(bottle);
                     }
                 });
             });
         }
     }
 
-    
+
+    /**
+     * Removes a throwable object (bottle) from the world.
+     * @param {ThrowableObject} bottle - The bottle to remove.
+     */
+    handleDeleteThrowableObject(bottle) {
+        const index = this.throwableObjects.indexOf(bottle);
+        if (index > -1) {
+            this.throwableObjects.splice(index, 1);
+        }
+    }
+
+
+    /**
+     * Checks if the player throws a bottle.
+     */
     checkThrowObjects() {
         if (this.keyboard.SPACE && this.character.bottlesCollected > 0 && this.character.allowThrow()) {;
             let bottle = new ThrowableObject(this.character.x + this.character.width/2, this.character.y + this.character.height / 3, this.character.otherDirection);
@@ -115,11 +175,17 @@ class World {
     }
 
 
+    /**
+     * Updates the status bar for bottles.
+     */
     updateStatusBarBottles() {
         this.statusBarBottles.setPercentage(this.character.bottlesCollected);
     }
 
 
+    /**
+     * Plays the sound for collecting a coin.
+     */
     playCollectCoinSound() {;
         this.get_coin_sound.currentTime = 0;
         this.get_coin_sound.volume = 0.5;
@@ -127,6 +193,10 @@ class World {
         this.get_coin_sound.play();
     }
 
+
+    /**
+     * Plays the sound for collecting a bottle.
+     */
     playCollectBottleSound() {
         this.get_bottle_sound.currentTime = 0;
         this.get_bottle_sound.volume = 0.5;
@@ -134,45 +204,99 @@ class World {
         this.get_bottle_sound.play();
     }
 
-    
+
+    /**
+     * Draws the game world and all objects.
+     */
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
-        let relationOfCameraToBackground = this.camera_x % (this.widthOfSingleBackground - this.positionCharacterInWorld_x );
-        
+        this.renderLevelElements();
+        this.renderBottlesInAir();
+        this.renderCharacter();
+        let self = this;
+        requestAnimationFrame(() => self.draw());
+        this.ctx.translate(-this.camera_x, 0);
+        this.renderStatusBars();
+    }
+    
+    
+    /**
+     * Renders all elements of the level, including background layers, clouds, enemies, coins, and bottles.
+     */
+    renderLevelElements() {
         if (this.level instanceof Level) {
-            if (relationOfCameraToBackground == 0) {
-                this.start_background_x_1 += this.widthOfSingleBackground * 2;
-                this.start_background_x_2 += this.widthOfSingleBackground * 2;
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/air.png', this.start_background_x_1));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', this.start_background_x_1));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', this.start_background_x_1));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', this.start_background_x_1));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/air.png', this.start_background_x_2));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', this.start_background_x_2));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', this.start_background_x_2));
-                this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', this.start_background_x_2));
-            }
-            
+            this.renderBackgroundLayers();
             this.addObjectsToMap(this.level.backgroundObjects);
             this.addObjectsToMap(this.level.clouds);
             this.addObjectsToMap(this.level.enemies);
             this.addObjectsToMap(this.level.coins);
             this.addObjectsToMap(this.level.bottles);
         }
+    }
+
+
+    /**
+     * Checks if the character has reached the endboss and updates the state accordingly.
+     */
+    renderBottlesInAir() {
         this.addObjectsToMap(this.throwableObjects);
+    }
+
+
+    /**
+     * Renders the character on the map.
+     */
+    renderCharacter() {
         this.addToMap(this.character);
-        
-        let self = this;
-        requestAnimationFrame(() => self.draw());
-        
-        this.ctx.translate(-this.camera_x, 0);
+    }
+
+
+    /**
+     * Renders the status bars for health, coins, and bottles.
+     */
+    renderStatusBars() {
         this.addToMap(this.statusBarHealth);
         this.addToMap(this.statusBarCoins);
         this.addToMap(this.statusBarBottles);
     }
 
+    
+    /**
+     * This function calculates the relation of the camera's x position to the character's 
+     * position in the world, which is used for rendering background layers.
+     * @returns {number} The relation of the camera's x position to the character's position.
+     */
+    getRelationOfCameraToCharacter() {
+        return this.camera_x % (this.widthOfSingleBackground - this.positionCharacterInWorld_x);
+    }
 
+
+    /**
+     * This function renders the background layers of the level, adding new layers as the camera moves to 
+     * create a continuous scrolling effect.
+     */
+    renderBackgroundLayers() {
+        let relationOfCameraToBackground = this.getRelationOfCameraToCharacter();
+        if (relationOfCameraToBackground == 0) {
+            this.start_background_x_1 += this.widthOfSingleBackground * 2;
+            this.start_background_x_2 += this.widthOfSingleBackground * 2;
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/air.png', this.start_background_x_1));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', this.start_background_x_1));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', this.start_background_x_1));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', this.start_background_x_1));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/air.png', this.start_background_x_2));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', this.start_background_x_2));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', this.start_background_x_2));
+            this.level.backgroundObjects.push(new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', this.start_background_x_2));
+        }
+    }
+
+
+    /**
+     * Adds an array of drawable objects to the map.
+     * @param {DrawableObject[]} objects - The objects to add.
+     */
     addObjectsToMap(objects) {
         objects.forEach(object => {
             this.addToMap(object);
@@ -180,6 +304,10 @@ class World {
     }
 
 
+    /**
+     * Adds a single drawable object to the map.
+     * @param {DrawableObject} drawableObject - The object to add.
+     */
     addToMap(drawableObject) {
         if (!(drawableObject instanceof StatusBar) && drawableObject.otherDirection) {
             this.flipImage(drawableObject);
@@ -191,8 +319,11 @@ class World {
             this.flipImageBack(drawableObject);
         }
     }
-    
-    
+
+
+    /**
+     * Sets up world references for character, sounds, and enemies.
+     */
     setWorld() {
         this.character.world = this;
         this.sounds.world = this;
@@ -202,8 +333,12 @@ class World {
             }
         });
     }
-    
-    
+
+
+    /**
+     * Flips the image of a moveable object for left direction.
+     * @param {MoveableObject} movableObject - The object to flip.
+     */
     flipImage(movableObject) {
         this.ctx.save();
         this.ctx.translate(movableObject.width, 0);
@@ -211,7 +346,11 @@ class World {
         movableObject.x = movableObject.x * -1;
     }
 
-    
+
+    /**
+     * Restores the image orientation after flipping.
+     * @param {MoveableObject} movableObject - The object to restore.
+     */
     flipImageBack(movableObject) {
         this.ctx.restore();
         movableObject.x = movableObject.x * -1;
