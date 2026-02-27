8a379679973c1e4ce4a695bc47c51440be4181f0
gloedige <gerd.loedige@posteo.de>
2026-02-27 06:42:07 +0100

    update level with clean code and js doc

--- models/level.class.js
+++ models/level.class.js
@@ -1,3 +1,6 @@
+/**
+ * Class representing a level in the game.
+ */
 class Level {
     enemies;
     backgroundObjects;
@@ -7,6 +10,15 @@ class Level {
     level_start_x = 100;
     level_end_x = 720 * 2 * 2 + 720;
 
+
+    /**
+     * Creates a new Level instance and initializes its properties.
+     * @param {Array<Object>} enemies - Array of enemy objects in the level.
+     * @param {Array<Object>} clouds - Array of cloud objects in the level.
+     * @param {Array<Object>} backgroundObjects - Array of background object instances in the level.
+     * @param {Array<Object>} coins - Array of coin objects in the level.
+     * @param {Array<Object>} bottles - Array of bottle objects in the level.
+     */
     constructor(enemies, clouds, backgroundObjects, coins, bottles) {
         this.enemies = enemies;
         this.backgroundObjects = backgroundObjects;
